cd70823d7b645f29c6e7f4d1b3d5f8f30a80d22c
mrvamps <vamps.linux@gmail.com>
2014-03-22 00:49:56 -0500

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,40 @@
+    Ext.define("OMV.module.admin.service.shoutcast.Settings", { // Define a new class
+    extend: "OMV.workspace.form.Panel", // What is the base type of this class
+
+    rpcService: "shoutcast", // Remote Procedure Call
+    rpcGetMethod: "getSettings", // Remote Procedure Call
+    rpcSetMethod: "setSettings", // Remote Procedure Call
+
+    getFormItems: function() { // Generic function for this class that initializes the GUI
+       return [{
+          xtype: "fieldset", // Type of the item
+          title: _("General settings"), // Text that is shown on the top edge of the fieldset
+          fieldDefaults: {
+             labelSeparator: ""
+          },
+          items: [{ // These items are inside the fieldset item defined above
+             xtype: "checkbox", // Type of the item
+             name: "enable", // Individual name of the item
+             fieldLabel: _("Enable"), // Text that is shown next to the checkbox. Keep this under 15 characters
+             checked: false // Default value if no settings have been applied yet, Try to change this to true
+             },
+             {
+             xtype: "numberfield", // Type of the item
+             name: "numberfield1", // Individual name of the item
+             fieldLabel: "Number", // Text that is shown next to the number field. Keep this under 15 characters
+             minValue: 0, // Self explanatory
+             allowDecimals: false, // Self explanatory
+             allowBlank: true // Self explanatory
+             }]
+          }];
+    }
+    });
+
+    // Register the class that is defined above
+    OMV.WorkspaceManager.registerPanel({
+    id: "Settings", //Individual id
+    path: "/service/shoutcast", // Parent folder in the navigation view
+    text: _("ShoutCast"), // Text to show on the tab , Shown only if multiple form panels
+    position: 10, // Horizontal position of this tab. Use when you have multiple tabs
+    className: "OMV.module.admin.service.shoutcast.Settings" // Same class name as defined above
+    });
