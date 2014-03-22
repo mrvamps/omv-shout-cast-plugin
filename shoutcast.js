7cb35c7b55e93ef9fc7a5b04626c642c8065423b
mrvamps <vamps.linux@gmail.com>
2014-03-22 00:50:21 -0500

    Create shoutcast.js

---
+++ shoutcast.js
@@ -0,0 +1,7 @@
+    OMV.WorkspaceManager.registerNode({
+    id: "shoutcast", // Individual ID
+    path: "/service", // Parent folder in the navigation view
+    text: _("ShoutCast DNAS"), // Text to show next to the icon
+    icon16: "images/ftp.png", // 16x16 pixel icon that is displayed in the navigation view
+    iconSvg: "images/ftp.svg" // SVG icon that is displayed in the navigation view
+    });
